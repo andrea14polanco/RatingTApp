e3d120765b7da83523c0f7ac614de8ac644741c4
Andrea Polanco <alpm9615@gmail.com>
2017-04-18 22:26:51 -0400

    Vistas nuevas y viejas actualizadas

---
+++ bin/static/app.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+var app = angular.module("RatingTApp", ["ngRoute"]);
+
+
+app.config(function($routeProvider, $locationProvider) {
+	 $locationProvider.html5Mode(true);
+	  $routeProvider
+			  .when("/Profesores", {
+			    templateUrl : "profesores/list.html"
+			  })
+			  .when("/Profesores/Create", {
+			    templateUrl : "profesores/nuevo-anadir-profesor.html"
+			  });
+	}
+
+);
+
+
+
